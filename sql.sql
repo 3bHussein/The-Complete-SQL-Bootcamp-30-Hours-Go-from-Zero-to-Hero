d6d994b4016250a092d8942eb74d3216a7b0ae4b
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-11 18:23:48 +0400

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,118 @@
+
+Run
+
+
+
+
+select 
+name as Nimo , *
+from 
+demo
+order BY name DESC;
+1
+​
+2
+--create table t1 (id int PRIMARY key ,name nvarchr(max))
+3
+--create table t1 (id int PRIMARY key );
+4
+create table t2 (id int PRIMARY key , name_t1 nvarchar)
+5
+create table t2 
+6
+SELECT * FROM t2;
+7
+select * from demo;
+8
+​
+9
+select 
+10
+name as Nimo , *
+11
+from 
+12
+demo
+13
+order BY name DESC;
+History
+MS SQL
+
+
+
+
+select 
+name as Nimo , *
+from 
+demo
+order BY name DESC;
+18:18:44
+MS SQL
+
+
+
+
+select 
+name as Nimo , *
+from 
+demo
+order BY name ASC;
+18:18:34
+MS SQL
+
+
+
+
+select 
+name as Nimo , *
+from 
+demo
+order BY name ASC;
+18:18:31
+MS SQL
+
+
+
+
+select 
+name as Nimo , *
+from 
+demo
+order BY name;
+18:18:15
+History
+About
+ Settings
+SQLite storage
+
+Memory
+
+ → "Create SQLite DB"
+I recommend using the latest version of Chrome for OpFS
+
+History storage
+
+Memory
+
+
+- clear old(30+ day) db history
+
+* сlose all other tabs with this site open, then refresh the page.
+
+Left Menu
+Editor
+Skins
+
+Night (dark)
+
+
+Color settings
+
+Table
+Hover cell
+None
+
+Format number
+None
+
+Old button panel
